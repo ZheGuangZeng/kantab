381f09ffe29322e1eead09a9771b061182cd6c66
Icebob <mereg.norbert@gmail.com>
2019-01-07 15:41:56 +0100

    remove label at login

--- frontend/components/account/Login.vue
+++ frontend/components/account/Login.vue
@@ -5,34 +5,35 @@
 				<v-card class="elevation-12" color="grey darken-4">
 
 					<div class="pa-4">
-						<v-card-title>
-							<span class="title">Sign In</span>
-						</v-card-title>
-						<v-card-text>
-							<v-form @submit.prevent="submit">
-								<div v-if="error" class="alert error">{{ error }}</div>
-								<div v-if="success" class="alert success">{{ success }}</div>
+						<form @submit.prevent="submit">
+							<Logo/>
+							<v-card-title>
+								<span class="title">Sign In</span>
+							</v-card-title>
+							<v-card-text>
+								<v-alert :value="error" type="error">{{ error }}</v-alert>
+								<v-alert :value="success" type="success">{{ success }}</v-alert>
 
-								<v-text-field name="email" v-model="email" label="E-mail or username" append-icon="person"></v-text-field>
-								<v-text-field name="password" type="password" v-model="password" label="Password" append-icon="lock"></v-text-field>
+								<v-text-field name="email" v-model="email" placeholder="E-mail or username" append-icon="person"></v-text-field>
+								<v-text-field name="password" type="password" v-model="password" placeholder="Password" append-icon="lock"></v-text-field>
+								<v-text-field v-if="need2FAToken" name="token" v-model="token" ref="token" label="Two-factor code" append-icon="unlock"></v-text-field>
+
+								<social-auth />
+
 								<div class="forgot">
 									<router-link to="/forgot-password">
 										<span class="caption" small>Forgot password?</span>
 									</router-link>
 								</div>
-								<v-text-field v-if="need2FAToken" name="token" v-model="token" ref="token" label="Two-factor code" append-icon="unlock"></v-text-field>
-
-								<social-auth />
-
 								<span class="caption">Don't have an account?
 									<router-link to="/signup">Sign Up</router-link>
 								</span>
-							</v-form>
-						</v-card-text>
-						<v-card-actions>
-							<v-spacer></v-spacer>
-							<v-btn color="primary" :loading="processing" type="submit" large>Login</v-btn>
-						</v-card-actions>
+							</v-card-text>
+							<v-card-actions>
+								<v-spacer></v-spacer>
+								<v-btn color="primary" :loading="processing" type="submit" large>Login</v-btn>
+							</v-card-actions>
+						</form>
 					</div>
 
 				</v-card>
@@ -83,8 +84,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-	.forgot {
-		margin-top: -24px;
-		text-align: right;
-	}
 </style>
